ef7819b0ff11638ab3586674716feca151d4bf6e
moruch4nn <moruch4nn@gmail.com>
2023-01-21 22:49:02 +0900

    ⚡️イベントリスナーのunregisterが可能に。

--- src/main/kotlin/dev/moru3/minepie/events/EventRegister.kt
+++ src/main/kotlin/dev/moru3/minepie/events/EventRegister.kt
@@ -3,14 +3,19 @@ package dev.moru3.minepie.events
 import org.bukkit.Bukkit
 import org.bukkit.event.Event
 import org.bukkit.event.EventPriority
+import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
 
 class EventRegister(val plugin: Plugin, runnable: EventRegister.()->Unit) {
     companion object {
-        val dummyListener = object: Listener { /**パス**/ }
-        inline fun <reified E: Event> Plugin.registerEvent(p: EventPriority = EventPriority.NORMAL, ic: Boolean = false, crossinline r: (E)->Unit) {
+        inline fun <reified E: Event> Plugin.registerEvent(p: EventPriority = EventPriority.NORMAL, ic: Boolean = false, crossinline r: (E)->Unit): RegistrationResult {
+            val dummyListener = object: Listener { /**パス**/ }
             Bukkit.getPluginManager().registerEvent(E::class.java, dummyListener, p, { _, e -> if(e is E) { r.invoke(e) } }, this, ic)
+            return RegistrationResult {
+                val handlers = E::class.java.getDeclaredMethod("getHandlerList").apply { isAccessible = true }.invoke(null) as HandlerList
+                handlers.unregister(dummyListener)
+            }
         }
     }
 
@@ -20,5 +25,9 @@ class EventRegister(val plugin: Plugin, runnable: EventRegister.()->Unit) {
 
     init {
         runnable.invoke(this)
+    }
+
+    class RegistrationResult(private val unregister: ()->Unit) {
+        fun unregister() { this.unregister.invoke() }
     }
 }
